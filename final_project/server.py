7c0e49a813ab7982de58a3319d7b3ca6587bda34
LuisKaizen75 <lc.garzon@uniandes.edu.co>
2022-11-07 18:10:23 -0500

    Some changes not added in the last commit

--- final_project/server.py
+++ final_project/server.py
@@ -6,19 +6,24 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
+    print("Translating...")
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    ans = translator.english_to_french(textToTranslate)
+    return ans
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
+    print("Translating...")
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    ans = translator.french_to_english(textToTranslate)
+    return ans
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
